a81bf0a707b147929b720772a84ab80d9f12ec93
jacweave <jacweave@indiana.edu>
2015-05-04 23:11:56 -0400

    Progress on TTT

--- assets/js/TTT.js
+++ assets/js/TTT.js
@@ -1,5 +1,7 @@
+var gameArray = new Array(3);
+var gameTurn = 0;
+
 $(document).ready(function(){
-    var gameTurn = 0;
     makeBoard();
     $(".gameCell").mouseover(function(){
 	$(this).css("background-color","gray");
@@ -11,7 +13,8 @@ $(document).ready(function(){
 
     $(".gameCell").click(function(event){
 	var pos = $(this).position();
-	alert(pos.left);
+	alert(pos.top);
+	//makeMove(pos.left % 100, pos.top % 100);
     });
 });
 
@@ -21,11 +24,10 @@ function makeBoard(){
     gameBoard.css("background-color","red");
     //gameBoard.css("z-index","0");
     $("h1").after(gameBoard);
-    var gameArray = new Array(3);
     for (var i = 0; i < 3; i++){
 	gameArray[i] = new Array(3);
 	for (var j = 0; j < 3; j++){
-	    gameArray[i][j] = {html:$("<div></div>"),x:i,y:j};
+	    gameArray[i][j] = {html:$("<div></div>"),checked:false,owner:null};
 	    gameBoard.append(gameArray[i][j].html);
 	    gameArray[i][j].html.addClass("gameCell");
 	    gameArray[i][j].html.css("background-color","blue");
@@ -37,5 +39,12 @@ function makeBoard(){
 		top: j * 100 + "px"
 	    });
 	}
+    }
+}
+
+function makeMove(x,y){
+    if (gameArray[x][y].owner === null){
+	gameArray[x][y].owner = gameTurn % 2;
+	gameTurn++;
     }
 }
