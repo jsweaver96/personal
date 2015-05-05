49734db701242b3410e5bc5534f85b12629d2b14
jacweave <jacweave@indiana.edu>
2015-05-04 21:23:29 -0400

    Start TTT

--- assets/js/TTT.js
+++ assets/js/TTT.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    var gameTurn = 0;
+    makeBoard();
     $(".gameCell").mouseover(function(){
 	$(this).css("background-color","gray");
     });
@@ -7,3 +9,10 @@ $(document).ready(function(){
 	$(this).css("background-color","white");
     });
 });
+
+function makeBoard(){
+    var gameBoard = $("<div></div>");
+    gameBoard.addClass("TTT");
+    gameBoard.css("background-color","red");
+    $("h1").after(gameBoard);
+}
