27fef34712259fe9ac94bb361d832bf4b2461459
jacweave <jacweave@indiana.edu>
2015-05-11 18:50:51 -0400

    Commenting snake code

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,54 +1,57 @@
-var running = false;
-var segSize = 10;
-var bxSize = 50;
-var bySize = 50;
-var boardArray = new Array(50);
-
+var running = false; // Is game running
+var segSize = 10; // Side length of each block on the game screen
+var bxSize = 50; // Number of blocks spanning across the game screen on the X axis
+var bySize = 50; // Number of blocks spanning across the game screen on the Y axis
+var boardArray = new Array(50); // Array holding the block objects that make up the game screen
+/* Possible statuses a position on the game screen */
 var openType = 1;
 var foodType = 2;
 var headType = 3;
 var bodyType = 4;
-
+/* Color values for the various block statuses */
 var openClr = "#FFFFFF";
 var foodClr = "#663300";
 var headClr = "#990000";
 var bodyClr = "#FF0000";
-
+/* Key values for up, down, left, and right */
 var kLeft = 37;
 var kUp = 38;
 var kRight = 39;
 var kDown = 40;
-
+/* Direction enums for the snake */
 var mLeft = 1;
 var mUp = 2;
 var mRight = 3;
 var mDown = 4;
-
-var snakeHead = new Object();
-var snakeBody = [];
-var bodySize = 2;
-var bodyToAdd = 0;
-var foodBit = new Object();
-
-var dir;
+/* Game objects */
+var snakeHead = new Object(); // Head of the snake
+var snakeBody = []; // Array of blocks that make up the snakes body
+var bodySize = 2; // Number of blocks in the body, starts at 2
+var bodyToAdd = 0; // Number of blocks to add to the body
+var foodBit = new Object(); // Piece of food on the game screen
+var dir; // Direction the snake is travelling
 
 $(document).ready(function(){
+    // Run game on clicking start button
     $("#startSnake").click(function(){
 	if (!running){
 	    gameStart();
 	}
     });
 
+    // Turn the start button dark gray when the mouse hovers over it
     $("#startSnake").mouseover(function(){
 	$(this).css("background-color","#4C4C4C");
     });
-    
+
+    // Restore the start button to its original color when the mouse leaves the button area
     $("#startSnake").mouseleave(function(){
 	if (!running){
 	    $(this).css("background-color","#BCBCBC");
 	}
     });
 
+    // When an arrow key is pressed, the snake's direction is changed if the game is running
     $(document).keydown(function(event){
 	if (running){
 	    changeDir(event.which);
@@ -56,11 +59,13 @@ $(document).ready(function(){
     });
 });
 
+// Initializes variables and sets the run speed for the game
 function gameStart(){
     gameInit();
     setInterval(function(){moveSnake();}, 50);
 }
 
+// Sets a block's type attribute to the given type and returns the block
 function setRect(seg, type){
     seg.type = type;
     if (seg.type == openType){
@@ -78,6 +83,7 @@ function setRect(seg, type){
     return seg;
 }
 
+// Sets the game state to running, sets up the game screen, adds a food bit, and sets up the snake
 function gameInit(){
     running = true;
     $("#startSnake").css("background-color","#4C4C4C");
@@ -106,6 +112,7 @@ function gameInit(){
     setFood();
 }
 
+// Changes direction the snake is headed based on the key input
 function changeDir(key){
     if (key == kLeft && snakeBody[0].x != snakeHead.x - 1){
 	dir = mLeft;
@@ -121,6 +128,7 @@ function changeDir(key){
     }
 }
 
+// Move the snake based on the current direction the snake is headed
 function moveSnake(){
     if (dir == mLeft){
 	shiftSnake(-1,0);
@@ -136,6 +144,7 @@ function moveSnake(){
     }
 }
 
+// Shifts a block based on the x and y scalar values
 function shiftSnake(x,y){
     if (snakeHead.x + x < 0 || snakeHead.x + x >= bxSize){
 	endGame();
@@ -166,6 +175,7 @@ function shiftSnake(x,y){
     }
 }
 
+// Checks if the snake head has collided with a food bit or with itself and reacts appropriately
 function checkEat(){
     if (snakeHead.x == foodBit.x && snakeHead.y == foodBit.y){
 	bodyToAdd = 1;
@@ -180,12 +190,14 @@ function checkEat(){
     }
 }
 
+// Ends the game and reloads
 function endGame(){
     running = 0;
     alert("You lose.");
     location.reload();
 }
 
+// Sets the location of the food bit and ensures that it does not spawn on the snake head or body
 function setFood(){
     var checked = false;
     while(!checked){
